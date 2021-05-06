aef871b700530631b66f4a6164cba249d94a3707
Emmanuel Sindani <crimsoftit@gmail.com>
2021-05-07 01:12:33 +0300

    15. company profile page build

--- deploy.py
+++ deploy.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/company_profile', methods=['GET'])
+def company_profile():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
