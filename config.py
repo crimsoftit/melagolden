b88698d0e9213c5ef8c38d3d3a998aaa1dd42db1
Emmanuel Sindani <crimsoftit@gmail.com>
2021-05-07 08:09:08 +0300

    17. contact page design

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 class BaseConfig (object):
     DEBUG = False
     SECRET_KEY = 'duaracoin'
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
--- deploy.py
+++ deploy.py
@@ -1,19 +1,29 @@
 from flask import Flask, redirect, render_template, url_for
+import os
 
 app = Flask(__name__)
-app.config.from_object('config.BaseConfig')
+app.config.from_object('config.DevelopmentConfig')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/company_profile', methods=['GET'])
+@app.route('/about/company_profile', methods=['GET'])
 def company_profile():
     return render_template('company_profile.html')
+
+@app.route('/about/faqs', methods=['GET'])
+def faq():
+    return render_template('faq.html')
+
+@app.route('/about/team', methods=['GET'])
+def team():
+    return render_template('team.html')
 
 @app.route('/contact', methods=['GET'])
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
