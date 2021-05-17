d3957c766457f303badb6b02f8b0b49b43f497f9
Crimsoft <crimsoftit@gmail.com>
2021-05-17 23:52:27 +0300

    49. still on UI templates

--- project/industries/views.py
+++ project/industries/views.py
@@ -12,26 +12,30 @@ def fashion():
 def real_estate():
     return render_template('real_estate.html')
 
-@industries_blueprint.route("/industries/retail_store", methods=['GET'])
+@industries_blueprint.route("/industries/retail_store/", methods=['GET'])
 def retail_store():
     return render_template('retail_store.html')
 
-@industries_blueprint.route("/industries/travel_agency", methods=['GET'])
+@industries_blueprint.route("/industries/travel_agency/", methods=['GET'])
 def travel_agency():
     return render_template('travel_agency.html')
 
-@industries_blueprint.route("/industries/cpa", methods=['GET'])
+@industries_blueprint.route("/industries/cpa/", methods=['GET'])
 def cpa():
     return render_template('cpa.html')
 
-@industries_blueprint.route("/industries/startups", methods=['GET'])
+@industries_blueprint.route("/industries/startups/", methods=['GET'])
 def startups():
     return render_template('startups.html')
 
-@industries_blueprint.route("/industries/restaurants", methods=['GET'])
+@industries_blueprint.route("/industries/restaurants/", methods=['GET'])
 def restaurants():
     return render_template('restaurants.html')
 
-@industries_blueprint.route("/industries/manufacturing", methods=['GET'])
+@industries_blueprint.route("/industries/manufacturing/", methods=['GET'])
 def manufacturing():
     return render_template('manufacturing.html')
+
+@industries_blueprint.route("/industries/amazon_sellers/", methods=['GET'])
+def amazon_sellers():
+    return render_template('amazon_sellers.html')
